5ba92e86de172029bad0a647c85df0c29c29b34a
Hritik Soni <76108554+hritiksoni22@users.noreply.github.com>
2021-06-23 13:39:49 +0530

    Add files via upload

---
+++ assistant.py
@@ -0,0 +1,64 @@
+import pyttsx3 
+import speech_recognition as sr
+import datetime
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning! time for breakfast")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon! time for lunch")   
+
+    else:
+        speak("Good Evening! go get some snacks")  
+
+    speak("I am your Virtual assistant")   
+    speak("hellooooooo")    
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+if __name__ == "__main__":
+    wishMe()
+    takeCommand()
+    
+
+
+
+
+
+
+
+
+
+   
